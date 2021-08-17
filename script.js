ab08f0e37de6cbb0fb69b4b02ebd4318878d51e3
kristinmc42 <mccollum.k.j@gmail.com>
2021-08-17 17:04:46 -0400

    added assignment of computer boats and start game button

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const app = {
-  boats: [{name:'carrier', length: 5}, {name: 'battleship', length: 4}, {name: 'cruiser', length: 3}, {name: 'submarine', length: 3}, {name: 'destroyer', length: 2}],
-  tryAgain: false,
+  // boats: [{name:'carrier', length: 5}, {name: 'battleship', length: 4}, {name: 'cruiser', length: 3}, {name: 'submarine', length: 3}, {name: 'destroyer', length: 2}],
+  
 
 };
+
+app.columnArray = ["a","b","c","d","e","f","g","h","i","j"];
+
 
 app.resetForms = () => {
   //reset all forms for setting boats
@@ -159,7 +162,21 @@ app.setBoats = (player) => {
     };
 
     app.setDestroyerButton = $('#submitDestroyer').attr('disabled', true);
+
+    // add text and button to start game
+    app.inputDiv = $('.input').append(`<h3>Let's go!</h3>`);
+    app.inputDiv = $('.input').append(`<button id="startGame">Start Game</button>`);
   });
+
+   // event listener for start game button
+   app.startGameButton = $('#startGame').on('click', function(e){
+    e.preventDefault();
+
+    console.log('start Game button pressed');
+    //hide player1's forms for setting boats
+    app.setFormElements = $('.setForm').hide();
+    app.inputH3Element = $('.input > h3:first').hide();
+   });
 
   //commented code below is for using prompt to get user input for setting boats
   //prompt box blocked the game board though so wasn't ideal
@@ -186,7 +203,7 @@ app.setBoats = (player) => {
   //   app.inputDiv = $('.input').append(`<p>${app.boats[i].name}  - Placed</p>`);
   // };
 
-}; // end of app.setBpats function
+}; // end of app.setBoats function
 
 app.setCarrier = () => {
     //    assign the text field to variable startingPosition
@@ -340,9 +357,7 @@ app.placeOnBoard = (shipArray, player) => {
   // turn column from a letter into a number
   // loop through 1-10 checking against alphabet
 
-  const columnArray = ["a","b","c","d","e","f","g","h","i","j"];
-
-  const convertedColumn = columnArray.indexOf(column) + 1;
+  const convertedColumn = app.columnArray.indexOf(column) + 1;
 
   console.log(column, convertedColumn, convertedRow);
 
@@ -369,12 +384,14 @@ app.placeOnBoard = (shipArray, player) => {
     };
     return true;
   }else if (!vertical && (convertedColumn + boatLength) <= 10){
-    console.log('assigning occupied to boat in row');
+    // assigning occupied to boat in row
 
      // checks if any of the squares already have class 'occuppied'
     // if yes, returns false and quits function
-    for (let a = 0; a < boatLength; a++){
-      if ($(`${player}.${column}${convertedRow + a}`).hasClass('occuppied')){
+    for (let b = 0; b < boatLength -1; b++){
+      column = app.columnArray[convertedColumn + b];
+
+      if ($(`${player}.${column}${convertedRow}`).hasClass('occuppied')){
         alert('Oops! That boat overlaps another. Please try again.');
         return false;
       };
@@ -383,9 +400,7 @@ app.placeOnBoard = (shipArray, player) => {
     // adds class 'occuppied' to each of the squares
     $(`${player}${startingPosition}`).addClass('occuppied');
     for (let j = 0; j < boatLength -1; j++){
-      column = columnArray[convertedColumn + j];
-
-      console.log(`${player}.${column}${convertedRow}`);
+      column = app.columnArray[convertedColumn + j];
 
       $(`${player}.${column}${convertedRow}`).addClass('occuppied');
     }; 
@@ -397,16 +412,95 @@ app.placeOnBoard = (shipArray, player) => {
   };
 };// end of placeOnBoard function
 
+
+app.setComputersBoats =  () => {
+  //set the computer's boats
+  // get boatlength from array
+  const boatArray = [5, 4, 3, 3, 2];
+
+  for (let a = 0; a < 5; a++){
+    // generate a starting position
+    let column = app.columnArray[Math.floor(Math.random() * 10)];
+    let row = Math.floor(Math.random() * 10) + 1;
+
+    // generate true/false for vertical
+    let vertical = Math.random() < 0.5;
+
+
+    // call placeOnBoard function (new function for computer)
+    // repeat for each boat in array
+    // if there is an overlap in placeOnBoard get new starting position and try again until no overlap
+
+    let continueGame = app.placeOnComputersBoard(column, row, vertical, boatArray[a]);
+
+    while (!continueGame) {
+      // generate new column and row and vertical and try again until boat can be set on board
+      // generate a starting position
+      let column = app.columnArray[Math.floor(Math.random() * 10)];
+      let row = Math.floor(Math.random() * 10) + 1;
+
+      // generate true/false for vertical
+      let vertical = Math.random() < 0.5;
+    
+      continueGame = app.placeOnComputersBoard(column, row, vertical, boatArray[a]);
+    };
+  };
+}; // end of app.setComputersBoats
+
+app.placeOnComputersBoard = (column, row, vertical, boatLength) => {
+  // convert letter of column to number
+  const convertedColumn = app.columnArray.indexOf(column) + 1;
+
+  if (vertical && (row + boatLength) <=10){
+    //assigning occuppied to boat in a column
+     // checks if any of the squares already have class 'occuppied'
+    // if yes, returns false and quits function
+    for (let a = 0; a < boatLength; a++){
+      if ($(`.player2.${column}${row + a}`).hasClass('occuppied')){
+        return false;
+      };
+    };
+    // adds class 'occuppied' to each of the squares
+    for (let i = 0; i < boatLength; i++){
+      $(`.player2.${column}${row + i}`).addClass('occuppied');
+      console.log(column, row +i, boatLength)
+    };
+    return true;
+  }else if (!vertical && (convertedColumn + boatLength) <= 10){
+      // assigning occupied to boat in row
+      // checks if any of the squares already have class 'occuppied'
+      // if yes, returns false and quits function
+      for (let b = 0; b < boatLength; b++){
+        column = app.columnArray[convertedColumn + b];
+
+        if ($(`.player2.${column}${row}`).hasClass('occuppied')){
+          return false;
+        };
+      };
+
+      // adds class 'occuppied' to each of the squares
+      for (let j = 0; j < boatLength; j++){
+        column = app.columnArray[convertedColumn + j];
+
+        $(`.player2.${column}${row}`).addClass('occuppied');
+        console.log(column, row, boatLength)
+      }; 
+      return true;
+  }else {
+     // condition for when boat length will be outside of the board dimensions
+     return false;
+  };
+};
+
 app.init = () => {
   // Hide h3 and forms for setting boats
   app.inputH3Element = $('.input h3').hide();
   app.setFormElements = $('.setForm').hide();
   
+  // event listener for when new game is clicked
   app.newGameButton = $('#newGame').on('click', function (e) {
-
     e.preventDefault();
 
-    // when newGame button clicked, 
     //reset forms for boat input
     app.resetForms();
 
@@ -414,14 +508,23 @@ app.init = () => {
     app.userName = prompt("What is your name?");
     app.h3UserName = $('#user').html(`(${app.userName}'s board)`);
 
-    //set the player's boats
-    app.setBoats('.player1');
+    //set the player's and computer's boats
 
-    //set the computer's boats
+    $.when(app.setBoats('.player1')).done(function() {
+      app.setComputersBoats();
+    });
+    
+    // // event listener for start game button
+    // app.startGameButton = $('#startGame').on('click', function(e){
+    //   e.preventDefault();
 
-
+    //   console.log('start Game button pressed');
+    //   //hide player1's forms for setting boats
+    //   app.setFormElements = $('.setForm').hide();
+    //   app.inputH3Element = $('.input > h3:first').hide();
+    // });
   });
-};
+}; // end of app.init
 
 $(function() {
   app.init();
